17c948110ce391e1b0e05b0961fc9069bd2cd7d7
nkeeping1996 <nfk.keeping@gmail.com>
2022-09-11 21:00:17 -0400

    New approach - mainly works, but need to iron out time constraints

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] digits = {9,8,7,6,5,4,3,9,9};
+
+        int[] dig = plusOne(digits);
+        for(int j : dig){
+            System.out.print(j);
+        }
+
     }
+
+    public static int[] plusOne(int[] digits) {
+        String s = "";
+        for(int i : digits){
+            s=s+ Integer.toString(i);
+        }
+        int i = Integer.parseInt(s);
+        i++;
+
+        s = Integer.toString(i);
+        String[] sArr = s.split("");
+
+        int[] addedArr= new int[s.length()];
+        for(int j = 0; j <= addedArr.length-1; j++){
+            addedArr[j] = Integer.parseInt(sArr[j]);
+        }
+
+        return addedArr;
+    }
+
 }
