869d2e187087c38b41ecb2612777430d3d6649ec
nkeeping1996 <nfk.keeping@gmail.com>
2022-09-12 20:24:42 -0400

    Final solution

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] digits = {9};
+        int[] digits = {1,2,9};
 
         int[] dig = plusOne(digits);
         for(int j : dig){
